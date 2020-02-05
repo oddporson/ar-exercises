fce69c0dd05e68a3af01ed42e524b8852226f709
Porson <porsonlee@gmail.com>
2020-02-05 01:06:45 +0000

    calculated total revenue of entire company. calculated the average annual revenue of all stores and check how many stores that are generating over 1 million dollar.

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@sum_of_revenue = Store.sum(:annual_revenue)
+puts "show me the money money money! --> #{@sum_of_revenue}."
+
+@avg_of_revenue = Store.average(:annual_revenue)
+puts "show me the average annual avenue --> #{@avg_of_revenue}."
+
+@num_of_stores = Store.where("annual_revenue >= 1000000").count
+puts "show the numbers of store that generates over $1,000,000 in annual revenue --> #{@num_of_stores}"
