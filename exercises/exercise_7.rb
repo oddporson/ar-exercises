540bb846d4af35679bd0761c28d686a6fd824c91
Porson <porsonlee@gmail.com>
2020-02-05 02:24:31 +0000

    created validations

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,13 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# Ask the user for a store name (store it in a variable)
+puts "Please give me a store name:"
+@store_name = gets.chomp
+
+# Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+@name_of_store = Store.create(name: @store_name)
+print @name_of_store.save
+
+
